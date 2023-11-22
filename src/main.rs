ef054121d79e9952cdb53461d36b2ed7eada1952
Kevin Hann <kevhann@gmail.com>
2023-11-22 00:09:19 -0500

    GUI for viewing JSON

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,175 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use egui::*;
 use serde_json::{Result, Value};
 
 fn main() -> Result<()> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-    {
-        "_id": "YIIUQOFH4QV9SGR0",
-        "name": "Belkis Langdon-Wiles",
-        "dob": "2017-06-30",
-        "address": {
-            "street": "8545 Rathmel Street",
-            "town": "Langport",
-            "postode": "MK1 8JM"
-        },
-        "telephone": "+965-9918-490-205",
-        "pets": [
-            "Loki",
-            "Lilly"
-        ],
-        "score": 5.7,
-        "email": "reagan4@hotmail.com",
-        "url": "http://five.com",
-        "description": "somewhat locking pmc guided jaguar rotary frequency explained difficulties representatives tramadol hot denver separation inflation urge environmental functions silent technical",
-        "verified": true,
-        "salary": 62045
-    }"#;
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
 
-    // Parse the string of data into serde_json::Value.
-    let mut v: Value = Value::default();
-     match serde_json::from_str(data) {
-        Ok(vv) =>v=vv,
-        Err(e) => println!("{}",e)
-     }
+    let mut tree = Tree::default();
+    let mut textb: String = "".to_string();
 
-    printItem(&v, "");
-    // Access parts of the data by indexing with square brackets.
+    let _ = eframe::run_simple_native("jsonv", options, move |ctx, _frame| {
+        ctx.set_visuals(egui::style::Visuals::dark());
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.set_min_size(ui.available_size());
+            tree = Tree::from_json(textb.as_str());
+
+            ui.heading("JSON Viewer");
+            ui.separator();
+            ui.with_layout(
+                egui::Layout::left_to_right(Align::Min).with_cross_justify(true),
+                |ui| {
+                    let h = ui.available_height();
+                    let w = ui.available_width() - 20.0;
+
+                    egui::ScrollArea::vertical()
+                        .id_source("first")
+                        .show(ui, |ui| {
+                            ui.set_min_height(h);
+                            ui.set_min_width(w / 2.0);
+                            tree.ui(ui);
+                        });
+                    ui.add(egui::Separator::default().vertical());
+                    egui::ScrollArea::vertical()
+                        .id_source("second")
+                        .show(ui, |ui| {
+                            ui.set_min_height(h);
+                            ui.set_min_width(w / 2.0);
+                            TextEdit::multiline(&mut textb)
+                                .min_size(Vec2 { x: w / 2.0, y: h })
+                                .ui(ui);
+                        });
+                },
+            );
+        });
+    });
 
     Ok(())
 }
 
+#[derive(Clone, Copy, PartialEq)]
+enum Action {
+    Keep,
+    Delete,
+}
 
-fn printItem(v:&Value,name:&str ){
-    match v {
-        Value::Object(ref m)=>
-        {            
-           for key in  m.keys(){
-                printItem(&v[key],key);
-           }
+#[derive(Clone, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+struct Tree {
+    children: Vec<Tree>,
+    name: String,
+    value: String,
+}
+
+impl Tree {
+    pub fn from_json(json: &str) -> Self {
+        let v = serde_json::from_str(json);
+
+        match v {
+            Ok(vv) => Self::from_value(&vv, "root"),
+            Err(_) => Self {
+                children: vec![],
+                name: "".to_string(),
+                value: "".to_string(),
+            },
         }
+    }
 
-        Value::String(ref s)=>{
-            println!("{name} : {s}");
-        }
-
-        Value::Number(ref n)=>{
-            println!("{name} : {n}");
-        }
-
-        Value::Array(ref a)=>{
-            println!("{name} :");
-            for item in a {
-                printItem(item,name);
+    pub fn from_value(v: &Value, name: &str) -> Self {
+        match v {
+            Value::Object(ref m) => {
+                let mut children = vec![];
+                for (key, value) in m {
+                    children.push(Self::from_value(value, key));
+                }
+                Self {
+                    children,
+                    name: name.to_string(),
+                    value: "".to_string(),
+                }
             }
+
+            Value::String(ref s) => Self {
+                children: vec![],
+                name: name.to_string(),
+                value: s.to_string(),
+            },
+
+            Value::Number(ref n) => Self {
+                children: vec![],
+                name: name.to_string(),
+                value: n.to_string(),
+            },
+
+            Value::Array(ref a) => {
+                let mut children = vec![];
+                for (i, item) in a.iter().enumerate() {
+                    children.push(Self::from_value(item, &format!("[{}]", i)));
+                }
+                Self {
+                    children,
+                    name: name.to_string(),
+                    value: "".to_string(),
+                }
+            }
+
+            Value::Bool(ref b) => Self {
+                children: vec![],
+                name: name.to_string(),
+                value: b.to_string(),
+            },
+            _ => Self {
+                children: vec![],
+                name: name.to_string(),
+                value: "".to_string(),
+            },
         }
-        _ => {}
-     }
+    }
+
+    pub fn ui(&mut self, ui: &mut Ui) -> Action {
+        self.ui_impl(ui, 0)
+    }
+}
+
+impl Tree {
+    fn ui_impl(&mut self, ui: &mut Ui, depth: usize) -> Action {
+        let n = self.name.clone();
+        let v = self.value.clone();
+        let t = if depth == 0 {
+            "root".to_string()
+        } else {
+            format!("{} : {}", n, v)
+        };
+
+        if self.children.is_empty() {
+            ui.label(t);
+            Action::Keep
+        } else {
+            CollapsingHeader::new(t)
+                .default_open(depth < 1)
+                .show(ui, |ui| self.children_ui(ui, depth))
+                .body_returned
+                .unwrap_or(Action::Keep)
+        }
+    }
+
+    fn children_ui(&mut self, ui: &mut Ui, depth: usize) -> Action {
+        self.children = std::mem::take(self)
+            .children
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, mut tree)| {
+                if tree.ui_impl(ui, depth + 1) == Action::Keep {
+                    Some(tree)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        Action::Keep
+    }
 }
